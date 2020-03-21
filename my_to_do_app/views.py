80b6aeefafdf8a2bb74c700bd97d270d4570e26b
djangoForPractice <cvd88@daum.net>
2020-03-21 10:23:22 +0900

    project init

---
+++ my_to_do_app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import *
+
+
+# Create your views here.
+
+
+def index(request):
+    todos = Todo.objects.all()
+    content = {'todos': todos}
+    return render(request, 'my_to_do_app/index.html', content)
+
+
+def createTodo(request):
+    user_input_str = request.POST['todoContent']
+    new_todo = Todo(content=user_input_str)
+    new_todo.save()
+    return HttpResponseRedirect(reverse('index'))
+    # return HttpResponse("create Todo를 할거야! =>"+user_input_str)
